e61cf6a3c2cad00cb9d5a4232db3decaca3ca496
Maykell Carrillo <maykellcarrillo@gmail.com>
2021-12-06 21:41:20 -0300

    boilerplate del repo

---
+++ example/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Button, ExampleComponent } from 'ui-components'
+import 'ui-components/dist/index.css'
+
+const App = () => {
+  return (
+    <>
+      <ExampleComponent text='Create React Library Example 😄' />
+      <Button />
+    </>
+  )
+}
+
+export default App
