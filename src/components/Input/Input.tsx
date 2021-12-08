4169aceadcff9918a0e951145f171bef8baaa3ca
Maykell Carrillo <maykellcarrillo@gmail.com>
2021-12-08 19:57:34 -0300

    actualiza config de prettier y agrega validaciones de inputs

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,32 @@
-import React, { FC, InputHTMLAttributes } from 'react'
-import './Input.css'
+import React, { FC, InputHTMLAttributes, ReactNode } from 'react'
+import styles from './Input.css'
 
-const Input: FC<InputProps> = ({ name, labelText, ...rest }) => {
+const Input: FC<InputProps> = ({
+  name,
+  labelText,
+  variant = 'default',
+  smallText,
+  icon,
+  ...rest
+}) => {
+  const { container, iconClass, inputContainer } = styles
   return (
-    <div>
+    <div className={container}>
       {labelText && <label htmlFor={name}>{labelText}</label>}
-      <input name={name} {...rest} />
+      <div className={inputContainer}>
+        <input className={styles[variant]} name={name} {...rest} />
+        {icon && <i className={iconClass}>{icon}</i>}
+      </div>
+      {smallText && <small>{smallText}</small>}
     </div>
   )
 }
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   labelText?: string
+  variant?: 'success' | 'error' | 'warning' | 'default'
+  icon?: ReactNode
+  smallText?: string
 }
 
 export default Input
