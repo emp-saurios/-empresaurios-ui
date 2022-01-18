539b324a986bde30da9061c6d89deb2630872f40
Maykell Carrillo <maykellcarrillo@gmail.com>
2022-01-17 22:48:14 -0300

    agrega variantes para separar estilos segun el tipo de variante

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,30 +1,37 @@
 import React, { FC, InputHTMLAttributes, ReactNode } from 'react'
 import styles from './Input.scss'
 
-const Input: FC<InputProps> = ({
-  name,
-  title,
-  variant,
-  message,
-  icon,
-  ...rest
-}) => {
-  const { icon_, input, label_, fill, message_ } = styles
+const Input: FC<InputProps> = ({ name, title, variant, message, icon, ...rest }) => {
+  const { icon_, input, label_, fill, message_, successMsg, errorMsg, warningMsg } = styles
+
+  const messageStyle: MessageStyle = {
+    success: successMsg,
+    error: errorMsg,
+    warning: warningMsg
+  }
+
   return (
     <div>
-      {title && <label className={label_} htmlFor={name}>{title}</label>}
+      {title && (
+        <label className={label_} htmlFor={name}>
+          {title}
+        </label>
+      )}
       <div className={input}>
         <input className={fill} name={name} {...rest} />
         {icon && <div className={icon_}>{icon}</div>}
       </div>
-      {message && <p className={message_}>{message}</p>}
+      {message && <p className={`${message_} ${messageStyle[(variant = 'success')]}`}>{message}</p>}
     </div>
   )
 }
 
+type Variants = 'success' | 'error' | 'warning'
+
+type MessageStyle = Record<Variants, string>
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   title?: string
-  variant?: 'success' | 'error' | 'warning'
+  variant?: Variants
   icon?: ReactNode
   message?: string
 }
