1de52115666abdde878ddad99c7b66052cb34e04
Alexis Mora Angulo <juanalexismoraangulo@hotmail.com>
2022-01-22 19:44:22 -0700

    Refactoriza código

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -3,7 +3,7 @@ import styles from './Switch.scss'
 
 const Switch: FC<SwitchProps> = ({active, type = 'checkbox', className = '', ...rest}) => {
 
-  return <input className={`${styles[type]} ${className}`} type="checkbox" defaultChecked={active} {...rest} />
+  return <input className={`${styles.input} ${styles[type]} ${className}`} type="checkbox" defaultChecked={active} {...rest} />
 }
 
 interface SwitchProps extends HTMLProps<HTMLInputElement> {
