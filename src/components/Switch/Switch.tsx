f5257adcc7cf5c41d0537628f126945c6670e079
Alexis Mora Angulo <juanalexismoraangulo@hotmail.com>
2022-01-22 19:24:22 -0700

    Agrega estilos para el toggle

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,9 +1,9 @@
 import React, {FC, HTMLProps} from 'react'
 import styles from './Switch.scss'
 
-const Switch: FC<SwitchProps> = ({active, type = 'toggle', className = '', ...rest}) => {
+const Switch: FC<SwitchProps> = ({active, type = 'checkbox', className = '', ...rest}) => {
 
-  return <input className={`${styles.input} ${styles[type]} ${className}`} type="checkbox" defaultChecked={active} {...rest} />
+  return <input className={`${styles[type]} ${className}`} type="checkbox" defaultChecked={active} {...rest} />
 }
 
 interface SwitchProps extends HTMLProps<HTMLInputElement> {
