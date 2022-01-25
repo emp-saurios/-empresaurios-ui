314e74631264967c0c9168fb77c0521dc9a062d4
Maykell Carrillo <maykellcarrillo@gmail.com>
2022-01-24 22:14:19 -0300

    agrega variante radio al componente switch

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,15 +1,15 @@
 import React, { FC, HTMLProps } from 'react'
 import styles from './Switch.css'
 
-const Switch: FC<SwitchProps> = ({ active, type = 'toggle', className = '', ...rest }) => {
+const Switch: FC<SwitchProps> = ({ type = 'toggle', className = '', ...rest }) => {
   const customClass = styles[type] + className
 
-  return <input className={customClass} type="checkbox" defaultChecked={active} {...rest} />
+  return <input className={customClass} type={type === 'radio' ? 'radio' : 'checkbox'} {...rest} />
 }
 
 interface SwitchProps extends HTMLProps<HTMLInputElement> {
   active?: boolean
-  type?: 'toggle' | 'checkbox'
+  type?: 'toggle' | 'checkbox' | 'radio'
 }
 
 export default Switch
