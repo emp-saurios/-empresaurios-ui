d7229239d0b2fbaa99314f63517cd01e72bc87cc
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-01-27 22:38:46 -0700

    se agregan props genéricas para mayor customizacion

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,16 +1,22 @@
 import React, { ChangeEvent, FC } from 'react'
 import Icon from '../Icon'
 
-const SearchBar: FC<SearchBarProps> = ({ value, onChange, placeholder = 'Buscar productos' }) => {
+const SearchBar: FC<SearchBarProps> = ({
+  value,
+  onChange,
+  placeholder = 'Buscar productos',
+  ...rest
+}) => {
   return (
     <form>
-      <input type="text" value={value} placeholder={placeholder} onChange={onChange} />
+      <input type="text" value={value} placeholder={placeholder} onChange={onChange} {...rest} />
       <Icon iconType="solid" icon="SearchIcon" />
     </form>
   )
 }
 
-interface SearchBarProps {
+interface SearchBarProps
+  extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void
   value: string
   placeholder?: string
